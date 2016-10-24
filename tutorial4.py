03436c412f097e31215081135a2be9d291331159
liama482 <liamabbate@hanovernorwichschools.org>
2016-10-24 13:59:15 -0400

    Updated from Brython Server: 10/24/2016 1:59:15 PM

--- tutorial4.py
+++ tutorial4.py
@@ -11,6 +11,30 @@ class SpaceShip(Sprite):
         Frame(227,0,292-227,125), 4, 'vertical')
     def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
+        self.thrust = 0
+        self.thrustframe = 1
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        SpaceGame.listenKeyEvent("keydown", "arrow", self.)
+        self.fxcenter = self.fycenter = 0.5
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
+    def thrustOn(self, event):
+        self.thrust = 1
+    def thrustOff(self, event):
+        self.thrust = 0
 
 class SpaceGame(App):
     #Tutorial4 space game example.
@@ -21,5 +45,8 @@ class SpaceGame(App):
         bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
         bg = Sprite(bg_asset, (0,0))
         SpaceShip((100,100))
+    def step(self):
+        for ship in self.getSpritesbyClass(SpaceShip):
+            ship.step()
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
